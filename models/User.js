9f931fe8dc0409610e04b6743c6396c62d95fe4d
nightdesert <87409279+nightdesert@users.noreply.github.com>
2023-11-24 21:24:09 +0530

    updated user model

--- models/User.js
+++ models/User.js
@@ -1,1 +1,48 @@
-const router = require("express").Router();
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const UserSchema = new mongoose.Schema({
+    _id: {
+        type: mongoose.Schema.Types.ObjectId
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    username: {
+        type: String,
+        require: true,
+        unique: true,
+        min: 3,
+        max: 20,
+    },
+    password: {
+        type: String,
+        require: true,
+        min: 6
+    },
+    name: {
+        type: String,
+        default: ""
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    }
+}, 
+{
+    timestamps: true
+});
+
+UserSchema.statics.login = async function(username, password) {
+    const user = await this.findOne({username});
+    if(user) {
+        const auth = await bcrypt.compare(password, user.password);
+        if(auth) return user;
+        throw Error('Incorrect Pasword')
+    }
+    throw Error('Incorrrect Username')
+}
+
+module.exports = mongoose.model("User", UserSchema)
